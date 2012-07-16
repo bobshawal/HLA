694a1ea8548b580ca8673bdb2b6d5bada4010c71
shawal sapuan <shawal.sapuan@infoconnect.com.my>
2012-07-16 17:45:08 +0800

    Frame

---
+++ HLA mobile/HLA mobile/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HLA mobile
+//
+//  Created by shawal sapuan on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HLA mobile/HLA mobile/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HLA mobile
+//
+//  Created by shawal sapuan on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
